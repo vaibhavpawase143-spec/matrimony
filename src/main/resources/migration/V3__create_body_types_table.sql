52c89b8a4413afeb0802e1ef6536874ae180ef03
Tanmay Hase <hasetanmay90@gmail.com>
2026-03-17 13:33:16 +0530

    hi

--- src/main/resources/migration/V3__create_body_types_table.sql
+++ src/main/resources/migration/V3__create_body_types_table.sql
@@ -1,17 +1,17 @@
 CREATE TABLE body_types (
 
-```
-id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
 
-admin_id BIGINT,
+    admin_id BIGINT,
 
-value VARCHAR(100),
+    value VARCHAR(100),
 
-status BOOLEAN DEFAULT TRUE,
+    status BOOLEAN DEFAULT TRUE,
 
-CONSTRAINT fk_bodytype_admin
-    FOREIGN KEY (admin_id)
-    REFERENCES admins(id)
-```
+    CONSTRAINT fk_bodytype_admin
+        FOREIGN KEY (admin_id)
+        REFERENCES admins(id),
+
+    CONSTRAINT uk_body_types_admin_value UNIQUE (admin_id, value)
 
 );
